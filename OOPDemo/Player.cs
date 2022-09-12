c6e33b94d02e276f1b0258116d23f41da5f71759
niklas-hjelm <niklas.hjelm@iths.se>
2022-09-12 11:30:25 +0200

    Add project files.

---
+++ OOPDemo/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    public class Player
+    {
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value.ToUpper();
+            }
+        }
+        
+        public int Level { get; set; }
+
+        public bool Alive { get; set; }
+    }
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using OOPDemo;
+
+Player niklas = new Player();
+
+niklas.Alive = true;
+niklas.Name = "Niklas";
+niklas.Level = 1;
+
+Console.WriteLine($"{niklas.Name} is level: {niklas.Level} and the player is alive: {niklas.Alive}");
